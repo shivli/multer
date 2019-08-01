d2e45e183d899eb0ef76b8c3adb1460cdce3d026
shivli <shivli.singh1994@gmail.com>
2019-08-01 15:00:51 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -2,5 +2,5 @@ module.exports = (app) => {
     const path = require('../controller/multer.js');
 
     // upload multiple files
-    app.post('/bulk', path.bulk);
+    app.post('/bulk/:id', path.bulk);
 }
